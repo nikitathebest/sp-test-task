efa012f5233b8e4599da0f5f7d972b1dd23b336a
Mikita Mishuta <45689290+nikitathebest@users.noreply.github.com>
2019-10-18 10:26:08 +0300

    Add specs for Webpage class

---
+++ spec/lib/webpage_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe Webpage do
+  let(:url) { '/home' }
+  let(:ip) { '127.0.0.0' }
+  let(:ips) { ['127.0.0.1', '127.0.0.1', '128.1.1.1'] }
+  
+  subject { described_class.new(url) }
+  
+  it 'updates webpage with ip and visit' do 
+    expect { subject.update_page(ip) }.to change { subject.visits }.by(1)
+    expect(subject.ips).to eq(['127.0.0.0'])
+  end
+end
